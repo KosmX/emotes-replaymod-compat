1e05c5ebbf8a8d76df96347d25b5a66deb79a4ec
KosmX <kosmx.mc@gmail.com>
2022-10-08 19:21:10 +0200

    Relay C2S to ReplayMod

--- src/main/kotlin/dev/kosmx/emotesCompat/rp/ClientInit.kt
+++ src/main/kotlin/dev/kosmx/emotesCompat/rp/ClientInit.kt
@@ -1,5 +1,6 @@
 package dev.kosmx.emotesCompat.rp
 
+import io.github.kosmx.emotes.api.events.client.ClientEmoteEvents
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -9,4 +10,10 @@ val LOGGER: Logger by lazy { LoggerFactory.getLogger(MODID) }
 
 fun initializeClient() {
     LOGGER.info("Hello Fabric")
+
+    //Save play emote c2s packet as s2c
+    ClientEmoteEvents.EMOTE_PLAY.register { emoteData, userID -> saveC2SEmotePacket(emoteData, userID) }
+    ClientEmoteEvents.LOCAL_EMOTE_STOP.register { saveC2SStopPacket() }
+
+
 }
---
+++ src/main/kotlin/dev/kosmx/emotesCompat/rp/PacketRecording.kt
@@ -0,0 +1,47 @@
+package dev.kosmx.emotesCompat.rp
+
+import com.replaymod.recording.ReplayModRecording
+import dev.kosmx.playerAnim.core.data.KeyframeAnimation
+import io.github.kosmx.emotes.api.proxy.INetworkInstance
+import io.github.kosmx.emotes.common.CommonData
+import io.github.kosmx.emotes.common.network.EmotePacket
+import io.netty.buffer.Unpooled
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
+import net.minecraft.client.MinecraftClient
+import net.minecraft.network.PacketByteBuf
+import net.minecraft.util.Identifier
+import java.util.UUID
+
+var currentId: UUID? = null
+
+fun saveC2SEmotePacket(emoteData: KeyframeAnimation?, player: UUID) {
+    if (MinecraftClient.getInstance().player?.uuid == player && emoteData != null) {
+        currentId = emoteData.uuid
+        ReplayModRecording.instance.connectionEventHandler.packetListener.save(
+            EmotePacket.Builder()
+                .configureToStreamEmote(emoteData, player)
+                .build().write().let { emotePacket ->
+                    val data = INetworkInstance.safeGetBytesFromBuffer(emotePacket)
+                    ServerPlayNetworking.createS2CPacket(Identifier(CommonData.MOD_ID, CommonData.playEmoteID),
+                        PacketByteBuf(Unpooled.buffer(data.size)).apply { this.writeBytes(data) })
+                }
+        )
+    }
+}
+
+fun saveC2SStopPacket() {
+    val player = MinecraftClient.getInstance().player?.uuid
+    if (MinecraftClient.getInstance().player?.isMainPlayer == true && player != null)
+    ReplayModRecording.instance.connectionEventHandler.packetListener.save(
+        EmotePacket.Builder()
+            .configureToSendStop(currentId, player)
+            .build().write().let {
+                val data = INetworkInstance.safeGetBytesFromBuffer(it)
+                ServerPlayNetworking.createS2CPacket(
+                    Identifier(CommonData.MOD_ID, CommonData.playEmoteID),
+                    PacketByteBuf(Unpooled.buffer(data.size).apply { this.writeBytes(data) })
+                )
+            }
+    )
+
+}
